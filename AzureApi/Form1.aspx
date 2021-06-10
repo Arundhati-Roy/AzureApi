c78a281e8a351c04e713eca9b61e158ba715da1a
Arundhati-Roy <arundhati.roy1998@gmail.com>
2021-06-10 20:09:32 +0530

    First commit

---
+++ AzureApi/Form1.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Form1.aspx.cs" Inherits="AzureApi.Form1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>
+                        Name:
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Email:
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Password:
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtPassword" runat="server" TextMode="Password">></asp:TextBox>
+                    </td>
+                    
+                </tr>
+                <tr>
+                    <td>
+
+                    </td>
+                    <td>
+                        <asp:Button ID="btnAdd" runat="server" Text="Add" OnClick="btnAdd_Click" />
+                        <asp:Button ID="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click" />
+                        <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <p>
+            &nbsp;</p>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ AzureApi/Form1.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web.Security;
+
+namespace AzureApi
+{
+    public partial class Form1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter sda = new SqlDataAdapter();
+        DataTable dt = new DataTable();
+        DataSet ds = new DataSet();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.ConnectionString= "Data Source=(LocalDB)\\RoyDB;Initial Catalog=AzureApi  DB;Integrated Security=True";
+            con.Open();
+            if(!Page.IsPostBack)
+            {
+                DataShow();
+            }
+        }
+
+        [Obsolete]
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+            string securepass = FormsAuthentication.HashPasswordForStoringInConfigFile(txtPassword.Text.ToString(), "MD5");
+            cmd.CommandText = "Insert into AzureTable (Name,Email,Password)values('"+txtName.Text.ToString()+"','"+txtEmail.Text.ToString()+"','"+txtPassword.Text.ToString()+"')";
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            
+            DataShow();
+        }
+        public void DataShow()
+        {
+            ds = new DataSet();
+            cmd.CommandText = " Select * From AzureTable";
+            cmd.Connection = con;
+            sda = new SqlDataAdapter(cmd);
+            sda.Fill(ds);
+            cmd.ExecuteNonQuery();
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+            con.Close();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+            cmd.CommandText = "Update AzureTable set Password='" + txtPassword.Text.ToString() + "',Email='" + txtEmail.Text.ToString() + "'where Name='" + txtName.Text.ToString() + "'";
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            DataShow();
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+            cmd.CommandText = "Delete from AzureTable where Name='" + txtName.Text.ToString() + "'";
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            DataShow();
+        }
+    }
+}
